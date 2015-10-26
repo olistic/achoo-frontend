c9de0e94691045c9b3388e499915137d2c711bd6
Matias Olivera <moliverafreire@gmail.com>
2015-10-26 13:31:30 -0300

    Setup an optimized build target

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 var path = require('path');
+var Clean = require('clean-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
 var merge = require('webpack-merge');
+
+var pkg = require('./package.json');
 
 var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
@@ -21,11 +25,6 @@ var common = {
   },
   module: {
     loaders: [
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-        include: APP_PATH
-      },
       {
         test: /\.jsx?$/,
         loaders: ['babel'],
@@ -49,8 +48,56 @@ if (TARGET === 'start' || !TARGET) {
       inline: true,
       progress: true
     },
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: APP_PATH
+        }
+      ]
+    },
     plugins: [
       new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+} else if (TARGET === 'build') {
+  module.exports = merge(common, {
+    entry: {
+      app: APP_PATH,
+      vendor: Object.keys(pkg.dependencies)
+    },
+    output: {
+      path: BUILD_PATH,
+      filename: '[name].[chunkhash].js?'
+    },
+    devtool: 'source-map',
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: APP_PATH
+        }
+      ]
+    },
+    plugins: [
+      new Clean(['build']),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.CommonsChunkPlugin(
+        'vendor',
+        '[name].[chunkhash].js'
+      ),
+      new ExtractTextPlugin('styles.[chunkhash].css'),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
     ]
   });
 }
