5a14592f58d8f3dd99af2cacbba7646042959b9b
alfredoelters <aelters@lithium.com.uy>
2015-11-08 20:14:49 -0200

    Merge

--- app/components/PharmacyItem.jsx
+++ app/components/PharmacyItem.jsx
@@ -14,9 +14,19 @@ export default class PharmacyItem extends Component {
     return (
       <div className="ListItem">
         <img className="ListItemImage" src={pharmacy.imageUrl}></img>
-        <Link to={`/pharmacies/${pharmacy.id}`}>
-          <div className="ListItemName">{pharmacy.name}</div>
-        </Link>
+        <div>
+          <Link to={`/pharmacies/${pharmacy.id}`}>
+            <div className="ListItemName">{pharmacy.name}</div>
+          </Link>
+          <p>{pharmacy.address}</p>
+        </div>
+        <span className="rating">
+              <label className="static-rating-star SELECTED" />
+              <label className="static-rating-star SELECTED" />
+              <label className="static-rating-star SELECTED" />
+              <label className="static-rating-star" />
+              <label className="static-rating-star" />
+        </span>
       </div>
     );
   }
